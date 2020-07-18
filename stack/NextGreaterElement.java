b1964d851b5e6494d9ff0345fff739ccf3a60614
biswajeet <biswajeetgp25@gmail.com>
2020-07-18 13:35:32 +0530

    print next greater element

---
+++ stack/NextGreaterElement.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+
+class NextGreaterElement {
+  public static void nextGreater(int arr[]) {
+    Stack<Integer> st = new Stack<>();
+    for (int i = 0; i < arr.length; i++) {
+      while (!st.isEmpty() && arr[i] > st.peek()) {
+        int popValue = st.pop();
+        System.out.println(popValue + "--> " + arr[i]);
+      }
+      st.push(arr[i]);
+    }
+    while(!st.isEmpty()){
+      int popValue =st.pop();
+      System.out.println(popValue+"--> "+-1);
+    }
+
+  }
+
+  public static void main(String args[]) {
+    int arr[] = { 2, 1, 3, 8, 6, 7, 5 };
+    nextGreater(arr);
+  }
+}
