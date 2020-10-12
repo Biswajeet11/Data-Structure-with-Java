627cadf4d521a793932fa23afc87c081ee99102d
Biswajeet Gope <biswajeetgp25@gmail.com>
2020-10-12 10:10:15 +0530

    added pattern1 and pattern2

---
+++ patternPrinting/Pattern1.java
@@ -0,0 +1,28 @@
+/*
+  Print the pattern :
+
+  *
+  * *
+  * * *
+  * * * *
+  * * * * *
+
+
+*/
+
+import java.util.Scanner;
+
+class Pattern1 {
+
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j <= i; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+
+  }
+}
