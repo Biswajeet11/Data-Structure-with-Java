89e3b96abc3bb9a5b2c7d17b3e979cb5429aa425
Biswajeet Gope <biswajeetgp25@gmail.com>
2020-10-12 15:39:06 +0530

    added 11, 12 ,13 pattern

--- patternPrinting/Pattern10.java
+++ patternPrinting/Pattern10.java
@@ -9,10 +9,9 @@ import java.util.Scanner;
 
 */
 
-
 public class Pattern10 {
   public static void main(String[] args) {
-    Scanner sc=new Scanner(System.in);
-    
+    Scanner sc = new Scanner(System.in);
+
   }
 }
