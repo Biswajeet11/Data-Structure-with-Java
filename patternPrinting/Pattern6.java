03694dd018bd2308f8337dee962e3aa0987e2f19
Biswajeet Gope <biswajeetgp25@gmail.com>
2020-10-12 12:56:25 +0530

    added pattern 6, 7 , 8

---
+++ patternPrinting/Pattern6.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+/* 
+
+*	*	*		*	*	*	
+*	*				*	*	
+*						*	
+*	*				*	*	
+*	*	*		*	*	*	
+
+*/
+
+public class Pattern6 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int stars = n / 2 + 1;
+    int spaces = 0;
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < stars; j++) {
+        System.out.print("*\t");
+      }
+      for (int j = 0; j < spaces; j++) {
+        System.out.print("\t");
+      }
+      for (int j = 0; j <stars; j++) {
+        System.out.print("*\t");
+      }
+      if (i < n/2) {
+        stars -= 1;
+        spaces += 2;
+      } else {
+        stars += 1;
+        spaces -= 2;
+      }
+      System.out.println();
+    }
+  }
+
+}
