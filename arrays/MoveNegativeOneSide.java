6445eaa03b9d0a39e795803f918f31a9741c3f07
Biswajeet Gope <biswajeetgp25@gmail.com>
2020-10-30 00:32:19 +0530

    added Move all negative numbers

--- arrays/MoveNegativeOneSide.java
+++ arrays/MoveNegativeOneSide.java
@@ -16,24 +16,43 @@ Output: -12 -13 -5 -7 -3 -6 11 6 5
 public class MoveNegativeOneSide {
 
   public static int[] moveNegative(int arr[]) {
-    for (int i = 0; i < arr.length; i++) {
-      if(arr[i]<0){
-        
+    int i = 0;
+    int j = 0;
 
+    while (i < arr.length) {
+      if (arr[i] < 0) {
+        swap(arr, i, j);
+        i++;
+        j++;
+      } else {
+        i++;
       }
     }
     return arr;
 
   }
 
+  public static int[] swap(int arr[], int i, int j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+    return arr;
+  }
+
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
+
     System.out.println("Enter the number of elements");
+    int n = sc.nextInt();
     int arr[] = new int[n];
 
     for (int i = 0; i < n; i++) {
       arr[i] = sc.nextInt();
+    }
+    int output[] = moveNegative(arr);
+    System.out.println("-------");
+    for (int i = 0; i < output.length; i++) {
+      System.out.println(output[i]);
     }
 
   }
